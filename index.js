42aa7404a45e6127c1228564088e46cc7031be43
Mateus Umbelino <mateusumbelino7@gmail.com>
2021-06-19 19:45:07 -0300

    Added Commando

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
+// const Discord = require('discord.js')
+// const client = new Discord.Client();
+
+const Commando = require('discord.js-commando')
 
 // const config = require('./config.json')
 const command = require('./command')
 
+const client = new Commando.CommandoClient({
+    owner: '143180142838022145',
+    commandPrefix: config.prefix
+})
+
 client.on('ready', () => {
     console.log('The client is ready!')
 
-    command(client, ['ping', 'test'], (message) => {
-        message.channel.send('Pong!')
-    })
+    client.registry.registerDefaults()
+
+    //command(client, ['ping', 'test'], (message) => {
+    //    message.channel.send('Pong!')
+    //})
 })
 
 client.login(process.env.DJS_TOKEN)
